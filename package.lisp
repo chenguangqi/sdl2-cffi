79cb64c5e3e8c685ed7bbff5d8304e7339c3c93c
mickbeaver <m.charles.beaver@gmail.com>
2014-08-29 10:20:17 -0500

    Removed unreferenced code in the example.

--- package.lisp
+++ package.lisp
@@ -1,6 +1,37 @@
 (defpackage #:sdl2-cffi
   (:use #:cl
-	#:cffi)
+        #:cffi)
   (:export #:+sdl-init-video+
-           #:sdl-get-version))
 
+           ;;SDL.h
+           #:sdl-init
+           #:sdl-quit
+
+           ;; SDL_version.h
+           #:major
+           #:minor
+           #:patch
+           #:sdl-version
+
+
+           ;; SDL_video.h
+           #:+sdl-windowpos-centered+
+           #:sdl-create-window
+           #:sdl-destroy-window
+           #:sdl-get-version
+           #:sdl-gl-context-debug-flag
+           #:sdl-gl-context-flag
+           #:sdl-gl-context-flags
+           #:sdl-gl-context-major-version
+           #:sdl-gl-context-minor-version
+           #:sdl-gl-create-context
+           #:sdl-gl-delete-context
+           #:sdl-gl-depth-size
+           #:sdl-gl-double-buffer
+           #:sdl-gl-set-attribute
+           #:sdl-gl-set-swap-interval
+           #:sdl-gl-swap-window
+           #:sdl-window
+           #:sdl-window-flags
+           #:sdl-window-opengl
+           #:sdl-window-shown))
